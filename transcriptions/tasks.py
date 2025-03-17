6cd613314bf5ea987538e0e55b09177baddffc02
Paweł Piękoś <pawel.piekos@center.ai>
2025-03-17 20:42:22 +0100

    Log failed transcription

--- transcriptions/tasks.py
+++ transcriptions/tasks.py
@@ -9,9 +9,10 @@ def transcribe(recording_transcription_id):
     recording_transcription = RecordingTranscriptions.objects.get(id=recording_transcription_id)
     try:
         transcription = get_transcription(recording_transcription)
-    except Exception:
+    except Exception as e:
         recording_transcription.status = RecordingTranscriptionStatus.FAILED.value
         recording_transcription.save()
+        print("Transcription failed", e)
         return
     recording_transcription.transcription = transcription["transcription"]
     recording_transcription.note = transcription["notes"]
